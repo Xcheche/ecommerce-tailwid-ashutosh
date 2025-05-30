ded5fc71ae8764a0df3bb606bcd7ec77982a33d6
xcheche <chekwubechukwu23@gmail.com>
2025-05-30 19:28:42 +0100

    home/products detail

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
 
 # Create your views here.
+def home(request):
+    try:
+        products = Product.objects.all()
+    except Product.DoesNotExist:
+        products = []
+    return render(request, 'main/index.html', {'products': products})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'main/product_detail.html', {'product': product})
