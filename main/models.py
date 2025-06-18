0a4a8b68e3c8dca355ce30d63199e4bf5a8d2c51
xcheche <chekwubechukwu23@gmail.com>
2025-06-18 16:27:21 +0100

    product page/card

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    product_image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, null=True)
     price = models.IntegerField()
     
 
